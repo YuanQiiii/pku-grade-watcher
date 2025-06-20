b679de1eb8c7587f43fd468a43643cb79692c562
shihuaidexianyu <shihuaidexianyu@gmail.com>
2025-06-20 04:54:16 +0000

    ✨ feat: 修正 check.sh 中的工作目录路径

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 显式地进入脚本所在的工作目录，确保后续命令的相对路径正确
-cd /home/xianyu/tasks/pku-grade-watcher-aq || exit
+cd /home/xianyu/tasks/pku-grade-watcher || exit
 
 # 使用 Conda 环境中 Python 解释器的绝对路径来执行脚本
 # 这样就不再需要 source 或 conda activate，对 cron 非常友好
